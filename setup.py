c07fb252f5240def1044e45a6d087d315d15fe1d
rohithgowdax <risedroyalruler@gmail.com>
2025-05-21 02:27:09 +0530

    add instructions

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rohith",
-    version="0.1.3",
-    packages=find_packages(),
+    name="rohith",                     # Replace with your package name
+    version="0.1.3",                   # Replace with your package version
+    packages=find_packages(),          # Automatically find packages in the current directory
     include_package_data=True,
-    install_requires=[
+    install_requires=[                  # These will be automatically installed when someone installs your package using pip
         "questionary",
         "rich",
         "requests"
     ],
-    entry_points={
+    entry_points={ 
         "console_scripts": [
-            "rohith=rohith.cli:main"
+            "rohith=rohith.cli:main"  # This points to the main() function inside the cli.py file within the rohith/ package.
         ]
     },
-    author="Rohith Gowda R",
-    description="A terminal-based CLI card to showcase Rohith Gowda's profile and quick contact tools.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/rohithgowdax/cli-card",  # Optional
+    author="Rohith Gowda R",        # Replace with your name
+    description="A terminal-based CLI card to showcase Rohith Gowda's profile and quick contact tools.",       # Replace with a short description of your package
+    # Make sure to have a README.md file in the same directory as this setup.py
+    long_description=open("README.md").read(),  # This will read the content of your README file and use it as the long description
+    long_description_content_type="text/markdown",  # Specify the format of the long description
+    url="https://github.com/rohithgowdax/cli-card",  # The URL of your package's repository
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
+        "Programming Language :: Python :: 3",      # Specify the Python versions you support
+        "License :: OSI Approved :: MIT License"    # Replace with the license you are using
     ],
 )
